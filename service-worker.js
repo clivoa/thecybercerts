7604ddca5f3b780bbc64fb9ec01590523053b1d3
Caio Lopes <caioviniciuslopes@gmail.com>
2026-03-15 13:22:51 +0100

    Update cache version to v8 in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "sec-cert-roadmap-v7";
+const CACHE_VERSION = "sec-cert-roadmap-v8";
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
