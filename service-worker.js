117b5b48a09a9047c33b0704f0bcf440053c0ded
Caio Lopes <caioviniciuslopes@gmail.com>
2026-07-07 01:29:47 +0200

    Bump cache version for updated index

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "sec-cert-roadmap-v14";
+const CACHE_VERSION = "sec-cert-roadmap-v15";
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
